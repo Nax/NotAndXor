5a3233aa1e8712f86b3d44ead44f59ac5e00b8fd
Nax <4265415+Nax@users.noreply.github.com>
2025-09-16 17:11:44 +0200

    Quick build script

---
+++ src/build.ts
@@ -0,0 +1,31 @@
+import path from 'path';
+import fs from 'fs/promises';
+import { Builder } from './gen/builder';
+import { build } from './gen/build';
+import { OutputFile } from './gen/types';
+
+async function buildProduction() {
+  const distPath = path.resolve(__dirname, '../dist');
+  if (await fs.stat(distPath).catch(() => false)) {
+    await fs.rm(distPath, { recursive: true });
+  }
+  await fs.mkdir(distPath, { recursive: true });
+
+  let promises: Promise<void>[] = [];
+
+  const writeFile = async (f: OutputFile) => {
+    const filePath = path.join(distPath, f.name);
+    const fileDir = path.dirname(filePath);
+    await fs.mkdir(fileDir, { recursive: true });
+    return fs.writeFile(filePath, f.content);
+  }
+
+  const builder = new Builder(f => promises.push(writeFile(f)));
+  await build(builder);
+  await Promise.all(promises);
+}
+
+buildProduction().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
