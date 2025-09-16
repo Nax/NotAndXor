d44639820d15fa038f59de24c5fe7ed37026e245
Nax <4265415+Nax@users.noreply.github.com>
2025-09-16 21:15:29 +0200

    Better

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,21 @@
 import { CONFIG } from '../gen/config';
-import type { ComponentChildren } from 'preact';
+import type { ComponentChildren, ComponentType } from 'preact';
 import { PageData } from '../gen/types';
+
+import { IconGithub, IconKoFi, IconLinkedin, IconRss, IconTwitch } from './Icons';
+
+type SocialLinkProps = {
+  href: string;
+  alt: string;
+  icon: ComponentType;
+};
+function SocialLink({ href, alt, icon: Icon }: SocialLinkProps) {
+  return (
+    <a href={href} target='_blank' rel='noopener noreferrer' aria-label={alt}>
+      <Icon/>
+    </a>
+  );
+}
 
 type LayoutProps = {
   data: PageData;
@@ -26,6 +41,13 @@ export function Layout({ data, children }: LayoutProps) {
         </header>
         <main>{children}</main>
         <footer>
+          <div class="social">
+            <SocialLink href="https://github.com/Nax" icon={IconGithub} alt="GitHub"/>
+            <SocialLink href="https://www.linkedin.com/in/mbacoux/" icon={IconLinkedin} alt="LinkedIn"/>
+            <SocialLink href="https://www.twitch.tv/naxunderscore" icon={IconTwitch} alt="Twitch"/>
+            <SocialLink href="https://ko-fi.com/nax__" icon={IconKoFi} alt="Ko-Fi"/>
+            <SocialLink href="/rss.xml" icon={IconRss} alt="RSS"/>
+          </div>
           <p>&copy; 2013 - {year} Maxime Bacoux.<br/>Some rights reserved.</p>
         </footer>
       </body>
