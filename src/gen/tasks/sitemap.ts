f3404f287b5327414e4826858aef90bbd8d510c1
Nax <4265415+Nax@users.noreply.github.com>
2026-03-31 23:16:03 +0200

    Use sitemap to generate, well, sitemaps

--- src/gen/tasks/sitemap.ts
+++ src/gen/tasks/sitemap.ts
@@ -1,50 +1,17 @@
+import { SitemapStream, streamToPromise, SitemapItemLoose } from 'sitemap';
+import { Readable } from 'stream';
+
 import { Article } from '../articles';
 import { Builder } from '../builder';
 import { CONFIG } from '../config';
-import { OutputFile } from '../types';
-import { JSDOM } from 'jsdom';
-import strftime from 'strftime';
 
-const NS = 'http://www.sitemaps.org/schemas/sitemap/0.9';
-
-function dateW3C(date: Date): string {
-  return strftime('%Y-%m-%d', date);
-}
-
-function addUrl(urlset: Element, loc: string, lastmod: Date, priority?: string) {
-  const document = urlset.ownerDocument;
-  const url = document.createElementNS(NS, 'url');
-  const locEl = document.createElementNS(NS, 'loc');
-  locEl.appendChild(document.createTextNode(loc));
-  url.appendChild(locEl);
-  const lastmodEl = document.createElementNS(NS, 'lastmod');
-  lastmodEl.appendChild(document.createTextNode(dateW3C(lastmod)));
-  url.appendChild(lastmodEl);
-  if (priority) {
-    const priorityEl = document.createElementNS(NS, 'priority');
-    priorityEl.appendChild(document.createTextNode(priority));
-    url.appendChild(priorityEl);
-  }
-  urlset.appendChild(url);
-}
-
-export async function buildSitemap(builder: Builder, articles: Article[]): Promise<OutputFile> {
-  /* Use JSDom to create the XML */
-  const { document } = (new JSDOM("<root/>", { contentType: "application/xml" })).window;
-  const root = document.documentElement;
-
-  const urlset = document.createElementNS(NS, 'urlset');
-  root.appendChild(urlset);
-
-  /* Index */
-  addUrl(urlset, CONFIG.baseUrl, new Date(), '1.0');
-
-  /* Articles */
+export async function buildSitemap(builder: Builder, articles: Article[]): Promise<void> {
+  const stream = new SitemapStream({ hostname: CONFIG.baseUrl });
+  const items: SitemapItemLoose[] = [];
+  items.push({ url: '/', lastmodISO: (new Date()).toISOString(), priority: 1.0 });
   for (const article of articles) {
-    addUrl(urlset, `${CONFIG.baseUrl}/${article.slug}`, article.updatedAt);
+    items.push({ url: `/${article.slug}`, lastmodISO: article.updatedAt.toISOString() });
   }
-
-  const xml = '<?xml version="1.0" encoding="UTF-8"?>' + root.innerHTML;
-
-  return builder.emit({ name: 'sitemap.xml', content: xml, mimeType: 'application/xml' });
+  const data = await streamToPromise(Readable.from(items).pipe(stream)).then(d => d.toString());
+  await builder.emit({ name: 'sitemap.xml', content: data, mimeType: 'application/xml' });
 }
