a789807dbb951e82b120b5e4e2de890d64d44fd1
Nax <4265415+Nax@users.noreply.github.com>
2026-04-01 21:52:26 +0200

    Fix font preloading (maybe)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -48,7 +48,7 @@ export function Layout({ data, children }: LayoutProps) {
         <meta name='viewport' content='width=device-width, initial-scale=1'/>
         {data.canonicalUrl && <link rel='canonical' href={data.canonicalUrl}/>}
         <title>{data.title}</title>
-        {data.fonts.map((f, i) => <link key={i} rel='preload' as='font' href={`/${f}`}/>)}
+        {data.fonts.map((f, i) => <link key={i} rel='preload' as='font' type="font/woff2" crossorigin='anonymous' href={`/${f}`}/>)}
         <link rel='stylesheet' href={`/${data.css}`}/>
         {data.meta.map((m, i) => <meta key={i} name={m.name} property={m.property} content={m.content}/>)}
         <script async src="https://scripts.simpleanalyticscdn.com/latest.js"/>
