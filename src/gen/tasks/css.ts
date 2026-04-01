e5090bf7538350ee55956d94ac896179b362615e
Nax <4265415+Nax@users.noreply.github.com>
2026-04-01 17:19:31 +0200

    Much, much better fonts

--- src/gen/tasks/css.ts
+++ src/gen/tasks/css.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
+import fs from 'node:fs/promises';
 import browserslist from 'browserslist';
 import { bundle, browserslistToTargets } from 'lightningcss';
 
@@ -9,17 +10,46 @@ import { OutputFile } from '../types';
 
 const targets = browserslistToTargets(browserslist());
 
+const depsMap = new Map<string, string>();
+const MIME_TYPES: Record<string, string> = {
+  '.woff': 'font/woff',
+  '.woff2': 'font/woff2',
+  '.ttf': 'font/ttf',
+  '.otf': 'font/otf',
+};
+
 export async function buildCss(builder: Builder): Promise<OutputFile> {
   const dirname = path.dirname(fileURLToPath(import.meta.url));
   const rootDir = path.resolve(dirname, '../../..');
   const inputFile = path.resolve(path.join(dirname, '../../index.css'));
 
-  const { code } = await bundle({
+  const { dependencies, code } = await bundle({
     projectRoot: rootDir,
     filename: inputFile,
     targets,
     minify: !CONFIG.dev,
+    analyzeDependencies: true,
   });
 
-  return builder.emit({ name: CONFIG.dev ? 'app.css' : 'app.[hash].min.css', content: code, mimeType: 'text/css' });
+  const urlDependencies = Object.fromEntries((dependencies || []).filter(d => d.type === 'url').map(d => [d.placeholder, path.resolve(rootDir, path.join(path.dirname(d.loc.filePath), d.url))]));
+  const newDeps = Object.keys(urlDependencies).filter(k => !depsMap.has(k));
+
+  /* Actually emit the fonts */
+  await Promise.all(newDeps.map(async key => {
+    const filePath = urlDependencies[key];
+    const content = await fs.readFile(filePath);
+    const ext = path.extname(filePath);
+    const mimeType = MIME_TYPES[ext] || 'application/octet-stream';
+    const outputFilePath = ['fonts/', CONFIG.dev ? path.basename(filePath) : '[hash]' + ext].join('');
+    const outputFile = builder.emit({ name: outputFilePath, content, mimeType });
+    depsMap.set(key, outputFile.name);
+  }));
+
+  /* Replace the placeholders */
+  let codeText = code.toString();
+  for (const [placeholder, outputPath] of depsMap.entries()) {
+    codeText = codeText.replaceAll(placeholder, '/' + outputPath);
+  }
+
+  return builder.emit({ name: CONFIG.dev ? 'app.css' : 'app.[hash].min.css', content: codeText, mimeType: 'text/css' });
 }
